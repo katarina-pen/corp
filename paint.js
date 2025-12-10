02fcef4c53a558b6cb7cd92d486076851421f58d
katarina-pen <katarinapenava0711@gmail.com>
2025-12-10 09:49:17 +0100

    v.50

--- paint.js
+++ paint.js
@@ -2,15 +2,16 @@ window.addEventListener('load', () => {
     // console.log("hello");
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
-    
+
     //Resizing
     // canvas.height = window.innerHeight;
     // canvas.width = window.innerWidth;
     canvas.height = canvas.offsetHeight;
     canvas.width = canvas.offsetWidth;
-    var color = "black"
+    let color = "black"
     ctx.strokeStyle = color;
     // ctx.strokeRect(50, 50, 200, 200);
+    
 
     //Variables
     let painting = false;
@@ -36,6 +37,7 @@ window.addEventListener('load', () => {
         ctx.strokeStyle = color;
         ctx.lineWidth = brushSize;
         ctx.lineCap = "round";
+        
         // console.log("målar")
         // ctx.lineTo(e.clientX, e.clientY); //musens position
         // ctx.stroke();
@@ -48,6 +50,17 @@ window.addEventListener('load', () => {
         ctx.stroke();
         ctx.beginPath();
         ctx.moveTo(x, y);
+        function createSquare() {
+            ctx.fillRect(x, y, 50, 50)
+        }
+
+        // squareBtn.addEventListener("click", createSquare);
+        // canvas.addEventListener("click", createSquare)
+       document.getElementById('squareBtn').onclick = function() {
+           canvas.addEventListener("click", createSquare)
+       }
+   
+
         
 
 
@@ -62,35 +75,108 @@ window.addEventListener('load', () => {
 
 
 
-    
+
     // brush slider
     const brushRange = document.querySelector("#brushRange");
     let brushSize = parseInt(brushRange.value);
     //texten där värdet på brush size står
     const brushOutput = document.querySelector("#brushOutput");
-brushOutput.textContent = brushSize
+    brushOutput.textContent = brushSize
 
-brushRange.addEventListener("input", () => {
-    brushSize = parseInt(brushRange.value);
-    brushOutput.textContent = brushSize;
-})
+    brushRange.addEventListener("input", () => {
+        brushSize = parseInt(brushRange.value);
+        brushOutput.textContent = brushSize;
+    })
 
-//Color change
-hotpinkBtn = document.querySelector("#hotpinkBtn");
-hotpinkBtn.addEventListener("click", colorChange);
+    //Color change
+    // HOTPINK
+    hotpinkBtn = document.querySelector("#hotpinkBtn");
+    hotpinkBtn.addEventListener("click", () => {
+        color = "hotpink";
+    });
 
-orangeBtn = document.querySelector("#orangeBtn");
-orangeBtn.addEventListener("click", colorChange);
+    // BLACK
+    blackBtn = document.querySelector("#blackBtn");
+    blackBtn.addEventListener("click", () => {
+        color = "black";
+    });
 
-function colorChange() {
-    // color = "hotpink";
+    //WHITE 
+    whiteBtn = document.querySelector("#whiteBtn");
+    //ERASE
+    eraseBtn = document.querySelector("#eraseBtn");
 
-    if (orangeBtn.PerformClick) {
-        color = "orange"
+    function whiteColor() {
+        color = "white";
     }
-}
+    eraseBtn.addEventListener("click", whiteColor);
+    whiteBtn.addEventListener("click", whiteColor);
 
-// const colorHotpink = document.querySelector("colorHotpink");
 
+    //RED 
+    redbtn = document.querySelector("#redBtn");
+    redBtn.addEventListener("click", () => {
+        color = "red";
+    });
+
+    //YELLOW 
+    yellowBtn = document.querySelector("#yellowBtn");
+    yellowBtn.addEventListener("click", () => {
+        color = "yellow";
+    });
+
+    //GREEN 
+    greenBtn = document.querySelector("#greenBtn");
+    greenBtn.addEventListener("click", () => {
+        color = "green";
+    });
+
+    //ORANGE 
+    orangeBtn = document.querySelector("#orangeBtn");
+    orangeBtn.addEventListener("click", () => {
+        color = "orange";
+    });
+
+    //GREY 
+    greyBtn = document.querySelector("#greyBtn");
+    greyBtn.addEventListener("click", () => {
+        color = "grey";
+    });
+
+    //PURPLE 
+    purpleBtn = document.querySelector("#purpleBtn");
+    purpleBtn.addEventListener("click", () => {
+        color = "purple";
+    });
+
+    //BROWN 
+    brownBtn = document.querySelector("#brownBtn");
+    brownBtn.addEventListener("click", () => {
+        color = "brown";
+    });
+
+    //BEIGE 
+    beigeBtn = document.querySelector("#beigeBtn");
+    beigeBtn.addEventListener("click", () => {
+        color = "beige";
+    });
+
+    //MYSTERY 
+    myseryBtn = document.querySelector("#myseryBtn");
+    myseryBtn.addEventListener("click", () => {
+        //SKA VARA SLUMPMÄSSIGT
+    
+    });
+
+    //Buttons and drawing functions
+    //ERASE finns längre upp med whiteBtn
+
+    //SHAPES
+    // squareBtn = document.querySelector("squareBtn");
+    // squareBtn.addEventListener("click",() => {
+    //     // ctx.fillRect(x,y,200,200);
+    //     console.log("square pressed");
+    // });
+    
 
 });
